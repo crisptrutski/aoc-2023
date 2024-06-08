0862f7d7c015b0c44092ba242bbfbfa6c3877309
Chris Truter <chris.truter.za@gmail.com>
2024-06-08 14:46:08 +0200

    create template

---
+++ src/template.clj
@@ -0,0 +1,22 @@
+(ns template
+  (:require [utils :as u]))
+
+(def example
+  "")
+
+(def input (slurp "DAY.txt"))
+
+
+(defn part-1 [i])
+
+
+
+(defn part-2 [i])
+
+
+(comment
+  (part-1 example)
+  (part-1 input)
+
+  (part-2 example)
+  (part-2 input))
