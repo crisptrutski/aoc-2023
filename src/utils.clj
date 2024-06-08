8d35d2bac8de494f7288204d501f0f148b7aab80
Chris Truter <chris.truter.za@gmail.com>
2024-06-08 14:48:01 +0200

    beef up utilities

--- src/utils.clj
+++ src/utils.clj
@@ -1,10 +1,19 @@
 (ns utils
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]))
 
-(defn tr [x] (clojure.pprint/pprint x) x)
+(defn tr [& x] (pprint (if (> (count x) 1) (vec x) (first x))) (last x))
+
+(defn tr- "disabled tr" [& x] (last x))
+
+(defn into! [xs ys]
+  (reduce conj! xs ys))
 
 (defn index-of [x xs]
   (first (keep-indexed (fn [i y] (if (= x y) i)) xs)))
+
+(defn every-odd [xs]
+  (map first (partition-all 2 xs)))
 
 (defn split-ws [s]
   (str/split (str/trim s) #"\s+"))
@@ -12,17 +21,47 @@
 (defn lines [s]
   (map str/trim (str/split-lines s)))
 
+(defn get-2d
+  ([grid [y x]]
+   (get-2d grid y x))
+  ([grid y x]
+   (-> grid (nth y []) (nth x nil))))
+
 (defn sum [xs]
-  (reduce + 0 xs))
+  (reduce + xs))
+
+(defn gcd [n d]
+  (if (> d n)
+    (recur d n)
+    (if (zero? d)
+      (abs n)
+      (recur d (mod n d)))))
+
+(defn lcm [n d]
+  (quot (abs (* n d)) (gcd n d)))
+
+(defn flip [f]
+  #(f %2 %1))
 
 (defn sort-desc [xs]
-  ;; todo - rather use custom comparator for speed
-  (reverse (sort xs)))
+  (sort (flip compare) xs))
 
 (defn capture
   ([re] (partial capture re))
   ([re s] (second (re-find re s))))
 
+(defn ending-with [e]
+  (fn [s]
+    (str/ends-with? s e)))
+
+(defn manhattan
+  "The manhattan distance between two points"
+  [[y1 x1] [y2 x2]]
+  (+ (abs (- x2 x1)) (abs (- y2 y1))))
+
 (defmacro match-idx [& preds]
   (let [indexed (map-indexed (fn [i p] [p i]) preds)]
     `(cond ~@(concat (apply concat indexed) [:else (count preds)]))))
+
+(defmacro printing [& body]
+  (cons 'do (for [b body] `(println ~b))))
